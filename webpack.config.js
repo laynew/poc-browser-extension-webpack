7d75f9f75ee25820c2edf22730576b87351b889d
Layne Wedderburn <layne@wedderburn.co.za>
2022-05-19 22:00:22 +0100

    Build and run project with web-ext
    Added web-ext-plugin which runs web-ext in webpack to build or run a
    web extension.
    
    Added build and start tasks to package.json.
    
    Running 'npm run start' will launch Firefox in debug mode with the
    extension loaded using web-ext via webpack+web-ext-plugin.

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const WebExtPlugin = require('web-ext-plugin');
+
+module.exports = {
+    entry: './src/extension/background_script.js',
+    output: {
+        filename: 'background_script.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                { from: "src/extension", to: "." },
+            ],
+        }),
+        new WebExtPlugin({
+            /**
+             * TODO: According to docs 'sourceDir' should be relative to source
+             * but is relative to package location in node_modules dir. Test if
+             * this behaviour is different on Linux.
+             * https://github.com/hiikezoe/web-ext-webpack-plugin/blob/master/README.md
+             */
+            sourceDir: '../../dist', 
+            browserConsole: true,
+            startUrl: 'about:debugging#/runtime/this-firefox',
+        }),
+    ],
+}
